79057aa95b3e1f196b04778de59ad7fd5917918f
Maro de Melo <marodemelo10@gmail.com>
2021-11-02 13:33:30 -0300

    Add project files.

---
+++ Introducao/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introducao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 9, 2, 3, 10, 5, 8, 7, 6, 1, 4 };
+
+            var resultado = from num in numeros
+                            where num > 4
+                            orderby num
+                            select num;
+
+            var resultado2 = numeros.Where(x => x > 4).OrderBy(x=>x);
+
+
+            foreach (var numero in resultado2)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
