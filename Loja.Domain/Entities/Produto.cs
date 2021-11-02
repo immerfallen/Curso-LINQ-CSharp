2d86c670828aa6b17aa3a5c4dd133cdf195fb3bf
Maro de Melo <marodemelo10@gmail.com>
2021-11-02 14:34:47 -0300

    Manipulando objetos complexos

---
+++ Loja.Domain/Entities/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Loja.Domain.Entities
+{
+    public class Produto
+    {
+        private List<Produto> _produto;
+        public Produto()
+        {
+            _produto = new List<Produto>();
+        }
+
+        public List<Produto> Listar()
+        {
+            _produto.Add(new Produto(){Id = Guid.NewGuid(),Nome = "Banana",Quantidade = 5,Valor = 5,DataVencimento = DateTime.Now.AddDays(5)});
+            _produto.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Melancia", Quantidade = 8, Valor = 6, DataVencimento = DateTime.Now.AddDays(2) });
+            _produto.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Manga", Quantidade = 2, Valor = 9, DataVencimento = DateTime.Now.AddDays(25) });
+            _produto.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Uva", Quantidade = 3, Valor = 25, DataVencimento = DateTime.Now.AddDays(9) });
+            _produto.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Pera", Quantidade = 1, Valor = 85, DataVencimento = DateTime.Now.AddDays(36) });
+            _produto.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Pessego", Quantidade = 2, Valor = 59, DataVencimento = DateTime.Now.AddDays(14) });
+            return _produto;
+        }
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public int Quantidade { get; set; }
+    public decimal Valor { get; set; }
+    public DateTime DataVencimento { get; set; }
+}
+}
---
+++ Loja/Program.cs
@@ -0,0 +1,27 @@
+﻿using Loja.Domain.Entities;
+using System;
+using System.Linq;
+
+namespace Loja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var produto = new Produto();
+            var produtos = produto.Listar().Where(p => p.Valor> 15);
+
+            foreach (Produto prod in produtos)
+            {
+                Console.WriteLine(prod.Nome + " " + prod.Valor);
+            }
+
+            new Produto().Listar().Where(x=>x.Valor<10).ToList().ForEach(x =>
+            {
+                Console.WriteLine(x.Nome);
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
