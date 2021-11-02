6fff9c11f63c6b0e68e62a96843e3fbf8d50654e
Maro de Melo <marodemelo10@gmail.com>
2021-11-02 13:42:56 -0300

    Explorando as clausulas select e where do linq

--- Introducao/Program.cs
+++ Introducao/Program.cs
@@ -10,22 +10,37 @@ namespace Introducao
     {
         static void Main(string[] args)
         {
-            int[] numeros = { 9, 2, 3, 10, 5, 8, 7, 6, 1, 4 };
+            //int[] numeros = { 9, 2, 3, 10, 5, 8, 7, 6, 1, 4 };
 
-            var resultado = from num in numeros
-                            where num > 4
-                            orderby num
-                            select num;
+            //var resultado = from num in numeros
+            //                where num > 4
+            //                orderby num
+            //                select num;
 
-            var resultado2 = numeros.Where(x => x > 4).OrderBy(x=>x);
+            //var resultado2 = numeros.Where(x => x > 4).OrderBy(x=>x);
 
 
-            foreach (var numero in resultado2)
+            //foreach (var numero in resultado2)
+            //{
+            //    Console.WriteLine(numero);
+            //}
+
+
+
+            string[] cores = { "preto", "vermelho", "azul", "verde", "branco" };
+
+            var resultadoCores = cores.Where(x => x.Contains("e") || x.Contains("z"));
+
+
+            foreach (string cor in resultadoCores)
             {
-                Console.WriteLine(numero);
+                Console.WriteLine(cor);
             }
+            
 
             Console.ReadKey();
         }
+
+        
     }
 }
