1a3bc3a563c1ebf1ef0bc12ef3db71a728fbe026
Maro de Melo <marodemelo10@gmail.com>
2021-11-03 09:04:45 -0300

    Manipulando objetos complexos com funções

--- Loja/Program.cs
+++ Loja/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Loja.Domain.Entities;
+using Newtonsoft.Json;
 using System;
 using System.Linq;
 
@@ -8,20 +9,59 @@ namespace Loja
     {
         static void Main(string[] args)
         {
-            var produto = new Produto();
-            var produtos = produto.Listar().Where(p => p.Valor> 15);
 
-            foreach (Produto prod in produtos)
-            {
-                Console.WriteLine(prod.Nome + " " + prod.Valor);
-            }
+            var produtos = new Produto();
+            produtos.Listar()
+            .Where(x => x.Nome.StartsWith("P") || x.Nome.EndsWith("a"))
+            .ToList()
+            .ForEach(x =>
+            //.Where(x => x.Nome.Contains("e"))
+            //.ToList()
+            //.ForEach(x =>
+                {
+                    Console.WriteLine(JsonConvert.SerializeObject(x));
+                    Console.WriteLine(" ");
 
-            new Produto().Listar().Where(x=>x.Valor<10).ToList().ForEach(x =>
+                });
+
+            var prods = new Produto().Listar();
+            var nomes = prods
+                .Where(x => x.Nome.StartsWith("P") || x.Nome.EndsWith("a"))
+                .Select(p => new ProdutoSelecionado { Name = p.Nome.Substring(1,2).Insert(2,"Maro").Remove(2,2).Replace("a", "L"), Value = p.Valor })
+                .ToList();
+            nomes.ForEach(item =>
             {
-                Console.WriteLine(x.Nome);
+                Console.WriteLine(JsonConvert.SerializeObject(item));
             });
 
+            var prod = new Produto().Listar();
+            var itens = prods
+                .Where(x => x.DataVencimento.Day>3)
+                .Select(p => new ProdutoSelecionado { Name = p.Nome, Value = p.Valor })
+                .ToList();
+            nomes.ForEach(item =>
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(item));
+            });
+
+            var pro = new Produto().Listar();
+            var it = pro
+                .Where(x => x.DataVencimento.Year == 2021)
+                .Select(p => new ProdutoSelecionado { Name = p.Nome, Value = p.Valor })
+                .ToList();
+            nomes.ForEach(item =>
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(item));
+            });
+
+
             Console.ReadKey();
+        }
+
+        public class ProdutoSelecionado
+        {
+            public string Name { get; set; }
+            public decimal Value { get; set; }
         }
     }
 }
